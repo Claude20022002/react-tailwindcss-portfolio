4d1fd69a593fb198309e7162f1d69debc2f5f8fb
Claude20022002 <clusamote@gmail.com>
2026-03-18 12:20:26 +0000

    Mise à jour des liens sociaux dans le pied de page et modification des crédits de copyright

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -1,69 +1,73 @@
 import {
-	FiGithub,
-	FiTwitter,
-	FiLinkedin,
-	FiGlobe,
-	FiYoutube,
-} from 'react-icons/fi';
-import AppFooterCopyright from './AppFooterCopyright';
+    FiGithub,
+    FiTwitter,
+    FiLinkedin,
+    FiGlobe,
+    FiYoutube,
+    FiFacebook,
+    FiInstagram,
+} from "react-icons/fi";
+import AppFooterCopyright from "./AppFooterCopyright";
 
 const socialLinks = [
-	{
-		id: 1,
-		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
-	},
-	{
-		id: 2,
-		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
-	},
-	{
-		id: 3,
-		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
-	},
-	{
-		id: 4,
-		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/in/realstoman',
-	},
-	{
-		id: 5,
-		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
-	},
+    {
+        id: 1,
+        icon: <FiInstagram />,
+        url: "https://www.instagram.com/claudia.lusamote?igsh=bTh2ZzE0Ymppazcz",
+    },
+    {
+        id: 2,
+        icon: <FiGithub />,
+        url: "https://github.com/Claude20022002",
+    },
+    {
+        id: 3,
+        icon: <FiFacebook />,
+        url: "https://www.facebook.com/share/1LAWsnW3oS/?mibextid=wwXIfr",
+    },
+    {
+        id: 4,
+        icon: <FiLinkedin />,
+        url: "https://www.linkedin.com/in/claudia-lusamote-kimfuta-271b512a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    },
+    /*
+    {
+        id: 5,
+        icon: <FiYoutube />,
+        url: "https://www.youtube.com/c/realstoman",
+    },
+	*/
 ];
 
 const AppFooter = () => {
-	return (
-		<div className="container mx-auto">
-			<div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
-				{/* Footer social links */}
-				<div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
-					<p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
-						Follow me
-					</p>
-					<ul className="flex gap-4 sm:gap-8">
-						{socialLinks.map((link) => (
-							<a
-								href={link.url}
-								target="__blank"
-								key={link.id}
-								className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
-							>
-								<i className="text-xl sm:text-2xl md:text-3xl">
-									{link.icon}
-								</i>
-							</a>
-						))}
-					</ul>
-				</div>
+    return (
+        <div className="container mx-auto">
+            <div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
+                {/* Footer social links */}
+                <div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
+                    <p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
+                        Suivez-moi
+                    </p>
+                    <ul className="flex gap-4 sm:gap-8">
+                        {socialLinks.map((link) => (
+                            <a
+                                href={link.url}
+                                target="__blank"
+                                key={link.id}
+                                className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
+                            >
+                                <i className="text-xl sm:text-2xl md:text-3xl">
+                                    {link.icon}
+                                </i>
+                            </a>
+                        ))}
+                    </ul>
+                </div>
 
-				<AppFooterCopyright />
-			</div>
-		</div>
-	);
+                <AppFooterCopyright />
+            </div>
+        </div>
+    );
 };
 
 export default AppFooter;
--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,26 +1,26 @@
 function AppFooterCopyright() {
-	return (
-		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<a
-					href="https://stoman.me"
-					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Stoman
-				</a>
-			</div>
-		</div>
-	);
+    return (
+        <div className="font-general-regular flex justify-center items-center text-center">
+            <div className="text-lg text-ternary-dark dark:text-ternary-light">
+                &copy; {new Date().getFullYear()}
+                <a
+                    href="https://github.com/Claude20022002/react-tailwindcss-portfolio"
+                    target="__blank"
+                    className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+                >
+                    React & Tailwind CSS Portfolio
+                </a>
+                .
+                <a
+                    href="https://www.linkedin.com/in/claudia-lusamote-kimfuta-271b512a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                    target="__blank"
+                    className="text-secondary-dark dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+                >
+                    Fait par Claude20022002
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default AppFooterCopyright;
