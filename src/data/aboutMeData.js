e57c4781744e7b53a117004b315135c90ae10759
Claude20022002 <clusamote@gmail.com>
2026-03-18 18:22:03 +0000

    Add Vite logo SVG asset to the project

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+    {
+        id: 1,
+        bio: "Au cours de mon parcours académique et professionnel, j’ai eu l’opportunité de participer à plusieurs expériences qui m’ont permis de développer des compétences techniques et humaines variées. J’ai notamment évolué dans des environnements collaboratifs où j’ai contribué à l’organisation logistique d’événements, au support informatique et à la mise en place de solutions technologiques. Ces expériences m’ont permis de renforcer mon sens de l’organisation, ma capacité d’adaptation et mon aptitude à travailler efficacement en équipe.",
+    },
+    {
+        id: 2,
+        bio: "Parallèlement, je me suis fortement investi dans des projets liés à la robotique, à la programmation et aux technologies numériques. À travers des stages, des projets académiques et des initiatives personnelles, j’ai pu approfondir mes connaissances en développement informatique, en automatisation et en vision par ordinateur. Passionné par l’innovation et l’apprentissage continu, je cherche constamment à améliorer mes compétences afin de concevoir des solutions technologiques utiles, performantes et adaptées aux besoins réels.",
+    },
 ];
--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,4 +1,5 @@
 // Import images
+/*
 import AmazonImage from '../images/brands/amazon_gray.png';
 import SonyImage from '../images/brands/sony_gray.png';
 import AdidasImage from '../images/brands/adidas_gray.png';
@@ -7,48 +8,29 @@ import NBImage from '../images/brands/nb_gray.png';
 import SamsungImage from '../images/brands/samsung_gray.png';
 import CanonImage from '../images/brands/canon_gray.png';
 import PumaImage from '../images/brands/puma_gray.png';
+*/
 
-export const clientsHeading = 'Some of the brands that trust me';
+export const clientsHeading = "Quelques langages que j'utilise";
 
 export const clientsData = [
-	{
-		id: 1,
-		title: 'Amazon',
-		img: AmazonImage,
-	},
-	{
-		id: 2,
-		title: 'Sony',
-		img: SonyImage,
-	},
-	{
-		id: 3,
-		title: 'Adidas',
-		img: AdidasImage,
-	},
-	{
-		id: 4,
-		title: 'FILA',
-		img: FilaImage,
-	},
-	{
-		id: 5,
-		title: 'NB',
-		img: NBImage,
-	},
-	{
-		id: 6,
-		title: 'SAMSUNG',
-		img: SamsungImage,
-	},
-	{
-		id: 7,
-		title: 'CANON',
-		img: CanonImage,
-	},
-	{
-		id: 8,
-		title: 'PUMA',
-		img: PumaImage,
-	},
+    { id: 1, title: "Python", img: "/assets/svg/python.svg" },
+    { id: 2, title: "React", img: "/assets/svg/react-js.svg" },
+    { id: 3, title: "JavaScript", img: "/assets/svg/javascript.svg" },
+    { id: 4, title: "HTML", img: "/assets/svg/html.svg" },
+    { id: 5, title: "CSS", img: "/assets/svg/css.svg" },
+    { id: 6, title: "MongoDB", img: "/assets/svg/mongodb.svg" },
+    { id: 7, title: "Ubuntu", img: "/assets/svg/ubuntu.svg" },
+    { id: 8, title: "PHP", img: "/assets/svg/php.svg" },
+    { id: 9, title: "Linux", img: "/assets/svg/linux.svg" },
+    { id: 10, title: "jQuery", img: "/assets/svg/jquery.svg" },
+    { id: 11, title: "Java", img: "/assets/svg/java.svg" },
+    { id: 12, title: "C", img: "/assets/svg/C.svg" },
+    { id: 13, title: "SASS", img: "/assets/svg/sass.svg" },
+    { id: 14, title: "TypeScript", img: "/assets/svg/typescript.svg" },
+    { id: 15, title: "Node.js", img: "/assets/svg/nodejs.svg" },
+    { id: 16, title: "Express", img: "/assets/svg/express.svg" },
+    { id: 17, title: "GitHub", img: "/assets/svg/github.svg" },
+    { id: 18, title: "MUI-JS", img: "/assets/svg/mui.svg" },
+    { id: 19, title: "Tailwind CSS", img: "/assets/svg/tailwind.svg" },
+    { id: 20, title: "MySQL", img: "/assets/svg/mysql.svg" },
 ];
