2efccb5b6537adda4c40cef5bf9ace1894cec11c
Claude20022002 <clusamote@gmail.com>
2026-03-18 12:10:01 +0000

    Fin de la première modification de AppBanner

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -18,7 +18,7 @@ const AppBanner = () => {
             transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
             className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
         >
-            <div className="w-full md:w-1/3 text-left">
+            <div className="w-full md:w-1/2 text-left">
                 <motion.h1
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
